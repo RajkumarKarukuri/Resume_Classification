8ab6b6e2a02029dd4b0b2f9f7fd8147fbef045c0
RajkumarKarukuri <karukurirajvarma58@gmail.com>
2025-07-29 07:43:18 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,7 +2,7 @@
  "cells": [
   {
    "cell_type": "code",
-   "execution_count": null,
+   "execution_count": Full,
    "id": "ee6f691a-d633-4eac-a2f8-384a36ced2e4",
    "metadata": {},
    "outputs": [],
