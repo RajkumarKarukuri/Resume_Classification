0752daaacf8154007508e082fd13cfca9b0851c7
RajkumarKarukuri <karukurirajvarma58@gmail.com>
2025-07-29 07:30:22 +0530

    Add files via upload

---
+++ app.py
@@ -0,0 +1,23 @@
+{
+ "cells": [
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "id": "ee6f691a-d633-4eac-a2f8-384a36ced2e4",
+   "metadata": {},
+   "outputs": [],
+   "source": []
+  }
+ ],
+ "metadata": {
+  "kernelspec": {
+   "display_name": "",
+   "name": ""
+  },
+  "language_info": {
+   "name": ""
+  }
+ },
+ "nbformat": 4,
+ "nbformat_minor": 5
+}
